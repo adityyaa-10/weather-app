84e5d0ec8d1988220ae06c23df209ad34d2fbdf6
Aditya <aditya.pandey.241103@gmail.com>
2024-01-22 18:56:00 +0530

    tweaks

--- scripts/app.js
+++ scripts/app.js
@@ -1,51 +1,59 @@
-const cityForm = document.querySelector('form')
-const card = document.querySelector('.card')
-const details = document.querySelector('.details')
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-    const cityDets = data.cityDets;
-    const weather = data.weather;
+    // destructure properties
+    const { cityDets, weather } = data;
 
+    // update details template
     details.innerHTML = `
-     <div class="text-muted text-uppercase text-center details">
-        <h5 class="my-3">${cityDets.EnglishName}</h5>
-        <div class="my-3">${weather.WeatherText}</div>
-        <div class="display-4 my-4">
-            <span>${weather.Temperature.Metric.Value}</span>
-            <span>&deg;C</span>
-        </div>
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
     </div>
-    `
+  `;
 
-    //Remove d-none class if present
+    // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+    if (weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
+
+    // remove the d-none class if present
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
-}
+};
 
 const updateCity = async (city) => {
 
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
+    return { cityDets, weather };
 
-    return {
-        cityDets: cityDets,
-        weather: weather
-    }
-}
+};
 
-
-cityForm.addEventListener('submit', (e) => {
+cityForm.addEventListener('submit', e => {
+    // prevent default action
     e.preventDefault();
 
-    //Get city value
-    const city = cityForm.city.value.trim()
+    // get city value
+    const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    //Update ui with new city
-    updateCity(city).then((data) => {
-        updateUI(data)
-    }).catch((err) => {
-        console.log(err);
-    })
-})
+    // update the ui with new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+});
